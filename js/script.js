b880e994c80a114ebb3e180a2b97442fd5197d85
Mohammad Suliman joya <92423023+joya66@users.noreply.github.com>
2022-06-28 22:41:23 +0430

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,10 +1,3 @@
-$(document).ready(function () {
-    // When the user clicks on a link in the navbar on smaller screens the navbar 
-       collapse.
-    $('.navbar-nav a').click(function(){
-            $(".navbar-collapse").collapse('hide');
-        });
-});
 
 //preloader
 
